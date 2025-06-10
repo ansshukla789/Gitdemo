64109abee1e000ef3c8dbe2399dc67c3f495bcd3
ansshukla789 <anshulshukla168@gmail.com>
2025-06-10 15:09:11 +0530

    App2 code changes

--- Demo_only/src/main/java/gitmit/Demo_only/App2.java
+++ Demo_only/src/main/java/gitmit/Demo_only/App2.java
@@ -4,6 +4,9 @@ public class App2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 System.out.println( "xyz" );
+		 System.out.println( "abx" );
+		 System.out.println( "fhffhffg" );
 
 	}
 
